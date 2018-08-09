91f6bb11a7d0b720def386861f205b3244b8b007
polyanin <polyanin@gmail.com>
2018-08-09 21:55:21 +0500

    update

---
+++ src/AplabAdminBundle.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: polyanin
+ * Date: 09.08.2018
+ * Time: 21:54
+ */
+
+namespace Aplab\AplabAdminBundle;
+
+
+class AplabAdminBundle extends Bundle
+{
+
+}
