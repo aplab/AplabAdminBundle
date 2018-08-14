1a8567415448c96b2af2f84080d29169249c00ad
polyanin <polyanin@gmail.com>
2018-08-14 14:56:25 +0500

    main menu in progress

---
+++ src/Component/MainMenu/Route.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: polyanin
+ * Date: 14.08.2018
+ * Time: 14:52
+ */
+
+namespace Aplab\AplabAdminBundle\Component\MainMenu;
+
+
+class Route extends Action
+{
+
+}
