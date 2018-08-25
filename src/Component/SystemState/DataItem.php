87644cbcf19ec5919599dea1a8313a63968d40d2
polyanin <polyanin@gmail.com>
2018-08-25 09:03:20 +0500

    introducing system state component

---
+++ src/Component/SystemState/DataItem.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: polyanin
+ * Date: 25.08.2018
+ * Time: 9:00
+ */
+
+namespace Aplab\AplabAdminBundle\Component\SystemState;
+
+
+class DataItem
+{
+
+}
---
+++ src/Component/SystemState/SystemStateManager.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: polyanin
+ * Date: 25.08.2018
+ * Time: 8:59
+ */
+
+namespace Aplab\AplabAdminBundle\Component\SystemState;
+
+
+class SystemStateManager
+{
+
+}
