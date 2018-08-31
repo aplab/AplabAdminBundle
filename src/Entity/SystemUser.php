1c67674fe659c12955f2b9b30252a57a3efe51b1
polyanin <polyanin@gmail.com>
2018-08-31 22:30:05 +0500

    add security bundle

---
+++ src/Entity/SystemUser.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: polyanin
+ * Date: 31.08.2018
+ * Time: 22:27
+ */
+
+namespace Aplab\AplabAdminBundle\Entity;
+
+
+class SystemUser implements UserInterface
+{
+
+}
